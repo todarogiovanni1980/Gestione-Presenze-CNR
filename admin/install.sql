46c3bdb38732904665d0c0e255f6a9568598ffe0
Giovanni Todaro <todarogiovanni1980@users.noreply.github.com>
2017-03-24 11:12:01 +0100

    Update install.sql

--- admin/install.sql
+++ admin/install.sql
@@ -1,12 +1,3 @@
-DROP TABLE IF EXISTS `#__todpre`;
- 
-CREATE TABLE `#__todpre` (
-  `id` int(11) unsigned NOT NULL auto_increment,
-  `greeting` varchar(25) NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
-
-
 DROP TABLE IF EXISTS `#__todpre_giustificativi`;
 
 CREATE TABLE `#__todpre_giustificativi` (
