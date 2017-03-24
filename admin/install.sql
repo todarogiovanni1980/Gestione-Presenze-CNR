dc513bb5f8d28c620101793ac4ed03d78ecd6222
Todaro Giovanni <giovanni.todaro@gmail.com>
2017-03-24 14:16:54 +0100

    Giustificativi Predefiniti
    Aggiunti alcuni giustificativi predefiniti

--- admin/install.sql
+++ admin/install.sql
@@ -8,3 +8,14 @@ CREATE TABLE `#__todpre_giustificativi` (
   `published` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
+
+INSERT INTO `#__todpre_giustificativi` (`id`, `catid`, `nome`, `codice`, `published`)
+VALUES
+	(1,4,'Assemblea Sindacale 1 ora','01',1),
+	(2,4,'Assemblea Sindacale 2 ore','02',1),
+	(3,4,'Assemblea Sindacale 3 ore','03',1),
+	(4,4,'Assemblea Sindacale 4 ore','04',1),
+	(5,4,'Assemblea Sindacale 5 ore','05',1),
+	(6,4,'Assemblea Sindacale 6 ore','06',1),
+	(7,4,'Assemblea Sindacale 7 ore','07',1),
+	(8,4,'Assemblea Sindacale 8 ore','08',1);
